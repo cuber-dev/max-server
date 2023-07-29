f82ae9ef27fd9fddb161a547dc8e36cb44ffc695
cuber-dev <118879829+cuber-dev@users.noreply.github.com>
2023-07-29 22:58:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const cors = require('cors')
 
 
