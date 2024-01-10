032efecc1f07fbbd0ee88d7bf252e2fcbda284cb
cuber-dev <adepunandhu9@gmail.com>
2024-01-10 13:26:13 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const fs = require('fs')
-const { getInfoRes, getFileName } = require('./utils')
+const { getInfoRes, getFileName, baseUrl } = require('./utils')
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors') 
@@ -23,25 +23,20 @@ app.get('/video', async (req, res) => {
       const choosenFormat = formats.find(format => format.qualityLabel === quality);
       if(choosenFormat){ 
         
-        // const headResponse = await axios.head(url);
-        // const contentLength = headResponse.headers['content-length'];
-        // console.log(contentLength)
-        // const fileSize = contentLength 
-
-        const duration = parseInt(choosenFormat.approxDurationMs) / 1000
-        const fileSize = (choosenFormat.bitrate / 8) * duration;
+        // const duration = parseInt(choosenFormat.approxDurationMs) / 1000
+        // const fileSize = (choosenFormat.bitrate / 8) * duration;
         const file = {
           format : choosenFormat,
           name : info.videoDetails.title,
           type : 'video', 
           qualityLabel : choosenFormat.qualityLabel,
           url : url,
-          size : choosenFormat.contentLength || fileSize,
+          size : choosenFormat.contentLength,
         } 
         
         res.setHeader('Content-Disposition', `attachment; filename="${getFileName(file.name,file.type)}"`);
         res.setHeader('Content-Type', 'video/mp4');
-        res.setHeader('Content-Length', file.size);
+        file.size && res.setHeader('Content-Length', file.size);
         console.log(file);
         
         ytdl(file.url, { format : file.format  }).pipe(res);
@@ -52,10 +47,41 @@ app.get('/video', async (req, res) => {
     res.status(500).send('Error getting video information');
   } 
 }); 
-
+app.get('/audio', async (req, res) => {
+  const { url , size} = req.query;
+  console.log("request : ",url)
+  try {
+    const info = await ytdl.getInfo(url);
+    if(info){
+      const formats = ytdl.filterFormats(info.formats, 'audioonly');
+      console.log(formats);
+      const choosenFormat = formats.find(format => format.contentLength === size);
+      if(choosenFormat){ 
+        const file = {
+          format : choosenFormat,
+          name : info.videoDetails.title,
+          type : 'audio', 
+          qualityLabel : choosenFormat.qualityLabel,
+          url : url,
+          size : choosenFormat.contentLength,
+        } 
+        
+        res.setHeader('Content-Disposition', `attachment; filename="${getFileName(file.name,file.type)}"`);
+        res.setHeader('Content-Type', 'audio/m4a');
+        file.size && res.setHeader('Content-Length', file.size);
+        console.log(file);
+        
+        ytdl(file.url, { format : file.format  }).pipe(res);
+      }else res.status(404).send('Video can not be downloadable with ',quality,'quality.');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error getting video information');
+  } 
+}); 
 app.get('/info',async (req, res) => {
   try {
-     const { url , type } = req.query;
+     const { url } = req.query;
     
     if (!ytdl.validateURL(url)) {
       const response = `URL: ${url} is an invalid YouTube video/short URL. Please provide a valid YouTube video/short URL.`
@@ -69,7 +95,7 @@ app.get('/info',async (req, res) => {
     }
 
     const info = await ytdl.getInfo(url);
-    res.json(getInfoRes(info,type)); 
+    res.json(getInfoRes(url,info)); 
   } catch (error) { 
     console.error('Error:', error);
     res.status(500).send('An error occurred while fetching audio info');
--- utils.js
+++ utils.js
@@ -1,4 +1,25 @@
 const ytdl = require('ytdl-core');
+const baseUrl = 'http://localhost:3000'
+const watermark = "[MAX] - "
+const getFileName = (filename,type) => `${watermark}${sanitizeFilename(filename)}.${type === 'audio' ? 'm4a' : 'mp4'}`
+const getBaseLinks = (type,url,file) => {
+    return type === 'audio' ? `${baseUrl}/audio?url=${url}&size=${file.size}` 
+        : `${baseUrl}/video?url=${url}&quality=${file.quality}`
+}  
+
+function getSizeLabel(size) {
+    if(!size) return 'unknown';
+
+    if (size < 1024) {
+        return `${size} bytes`;
+      } else if (size < 1048576) {
+        return `${(size / 1024).toFixed(2)} KB`;
+      } else if (size < 1073741824) {
+        return `${(size / 1048576).toFixed(2)} MB`;
+      } else {
+        return `${(size / 1073741824).toFixed(2)} GB`;
+      }
+  }
 
 function sanitizeFilename(filename) {
     return filename
@@ -7,50 +28,58 @@ function sanitizeFilename(filename) {
   }
   
   
-  function storeUserQuery(req, res, next){
-      const { url , type } = req.query;
-      const time = new Date();
-      const hours = time.getHours().toString()
-      const minutes = time.getMinutes().toString()
-      const seconds = time.getSeconds().toString()
-      const timeLabel = `${hours}:${minutes}:${seconds}`;
+//   function storeUserQuery(req, res, next){
+//       const { url , type } = req.query;
+//       const time = new Date();
+//       const hours = time.getHours().toString()
+//       const minutes = time.getMinutes().toString()
+//       const seconds = time.getSeconds().toString()
+//       const timeLabel = `${hours}:${minutes}:${seconds}`;
       
-      const date = new Intl.DateTimeFormat('en-US').format(time);
+//       const date = new Intl.DateTimeFormat('en-US').format(time);
       
-      const dateTimeLabel = `${date} ${timeLabel}`;
+//       const dateTimeLabel = `${date} ${timeLabel}`;
       
-      const userQuery = `${type} - ${url} - ${dateTimeLabel} \n`
+//       const userQuery = `${type} - ${url} - ${dateTimeLabel} \n`
       
-      userQueries.push({
-        userId : "1",
-        query : userQuery
-      })
-      fs.writeFileSync('./static/userQueries.json', JSON.stringify(userQueries)) 
-      console.log("Total requests : ",userQueries.length)
-      next();
-  }
+//       userQueries.push({
+//         userId : "1",
+//         query : userQuery
+//       })
+//       fs.writeFileSync('./static/userQueries.json', JSON.stringify(userQueries)) 
+//       console.log("Total requests : ",userQueries.length)
+//       next();
+//   }
 
-const watermark = "[MAX] - "
-const getFileName = (filename,type) => `${watermark}${sanitizeFilename(filename)}.${type === 'audio' ? 'm4a' : 'mp4'}`
-  
-const getInfoRes = (info,type) => {
+const getInfoRes = (url,info) => {
     let videoFormats = ytdl.filterFormats(info.formats, 'audioandvideo');
     let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
     console.log('video : ',videoFormats)
     console.log('audio : ',audioFormats)
-
+    const videoLinks = videoFormats.map((format,i) => ({
+        id : i,
+        link : getBaseLinks('video',url,{ quality : format.qualityLabel}),
+        size : format.contentLength,
+        sizeLabel : getSizeLabel(format.contentLength),
+        qualityLabel : format.qualityLabel
+    }))
+    const audioLinks = audioFormats.map((format,i) => ({
+        id : i,
+        link : getBaseLinks('audio',url,{ size : format.contentLength}),
+        size : format.contentLength,
+        sizeLabel : getSizeLabel(format.contentLength),
+    })) 
     const response = {
-        videoFormats,
-        audioFormats,
-        generalInfo : { 
-          title : info.videoDetails.title,
-          fileName : getFileName(info.videoDetails.title,type),
-        },
+        title : info.videoDetails.title,
+        thumbnail : info.videoDetails.thumbnails[info.videoDetails.thumbnails.length - 1].url,
+        audioLinks,
+        videoLinks,
       } 
     return response;
 }
 
 module.exports = {
     getInfoRes,
-    getFileName
+    getFileName,
+    baseUrl,
 }
